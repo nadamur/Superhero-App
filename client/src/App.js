4cb128bfbab953c16b77fe8ff611db571ed0679f
nadamur <nmurad4@uwo.ca>
2023-11-30 17:15:01 -0500

    selection fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './App.css'; // Import your CSS file
 import LogIn from './LogIn.js';
@@ -15,10 +15,13 @@ function App() {
   const [errorMessage, setErrorMessage] = useState('Your Search Results will be displayed here...');
   //temporary array to hold search results
   const listItems = [];
+  //temporary array to hold selected results
+  const selectedListItems = [];
   //sorting lists
   const [sortedSearchResults, setSortedSearchResults] = useState([]);
   //selection
   const [selectedResults, setSelectedResults] = useState([]);
+  const selectedItemsRef = useRef([]);
   //fav lists
   const [favoriteLists, setFavoriteLists] = useState([]);
   const [listNameToDelete, setListNameToDelete] = useState([]);
@@ -28,12 +31,12 @@ function App() {
     setListNameToAdd(favoriteLists.length > 0 ? favoriteLists[0] : '');
     getFavLists();
     displaySearch();
-  }, [searchResults, listNameToAdd, favoriteLists]);
+    console.log(selectedResults);
+  }, [searchResults, listNameToAdd, favoriteLists,selectedResults]);
 
   //function to add selected items to a fav list
-  const addSelectedHeroesToList= async () =>{
-    console.log('r:' + selectedResults);
-    console.log('l:' + listNameToAdd)
+  const addSelectedHeroesToList= async (event) =>{
+    event.preventDefault();
     const url = `/api/lists/add/${listNameToAdd}?ids=${selectedResults}`;
     try{
       const response = await fetch(url, {
@@ -41,6 +44,12 @@ function App() {
       });
       if (response.status === 200) {
         console.log('List created successfully');
+        // clear selectedItemsRef
+        selectedItemsRef.current.forEach((selectedItem) => {
+          selectedItem.classList.remove('selected');
+        });
+        selectedItemsRef.current = [];
+        setSelectedResults([]);
       } else if (response.status === 404) {
         console.log('List name already exists');
       } else {
@@ -52,22 +61,37 @@ function App() {
   }
 
   //function to add a result to 'selectedResults' list once it has been selected
-  const selectResults= (event) =>{
-    const listItem = event?.target?.closest("li");
-    if(listItem){
+  const selectResults = (event) => {
+    event.preventDefault();
+    const listItem = event?.target?.closest('li');
+    if (listItem) {
       const id = listItem.id;
-      setSelectedResults((prevSelectedResults) =>{
-        if (prevSelectedResults.includes(id)){
-          listItem.classList.remove("selected");
+      setSelectedResults((prevSelectedResults) => {
+        if (prevSelectedResults.includes(id)) {
+          listItem.classList.remove('selected');
+          // Remove the element from selectedItemsRef
+          selectedItemsRef.current = selectedItemsRef.current.filter(
+            (selectedItem) => selectedItem !== listItem
+          );
           return prevSelectedResults.filter((itemId) => itemId !== id);
-        }
-        else{
-          listItem.classList.add("selected");
+        } else {
+          listItem.classList.add('selected');
+          // Add the element to selectedItemsRef
+          selectedItemsRef.current = [...selectedItemsRef.current, listItem];
           return [...prevSelectedResults, id];
         }
       });
     }
   }
+//function to remove all items from selected list
+  const resetSelectedResults= () =>{
+    selectedListItems.forEach((result)=>{
+      result.classList.remove("selected");
+    })
+    setSelectedResults([]);
+  }
+
+  //changes listNameToAdd variable to the currently selected list
   const listAddNameChange= (event) =>{
     const selectedList = event.target.value;
     setListNameToAdd(selectedList);
@@ -93,6 +117,7 @@ function App() {
       console.error('Error fetching favorite lists:', error);
     }
   };
+
   // helper function to compare two arrays -- to stop 'favoriteLists' from being updated in a loop
   const arraysAreEqual = (array1, array2) => {
     if (array1.length !== array2.length) {
@@ -155,7 +180,6 @@ function App() {
     }
   }
 
-
   //function to retrieve ids of heroes that match search
   const searchSuperheroes = async (criteria) => {
     try {
@@ -172,7 +196,6 @@ function App() {
         setErrorMessage('Loading...');
         await displayHeroes(data);
         setErrorMessage('');
-        
       }
     } catch (error) {
       console.error('Error:', error);
@@ -203,7 +226,7 @@ function App() {
         ));
   
       let listItem = (
-        <li key={i} id={i} name={hero.name} race={hero.Race} publisher={hero.Publisher}>
+        <li key={i} id={i} name={hero.name} race={hero.Race} publisher={hero.Publisher} className ={selectedResults.includes(i) ? 'selected' : ''}>
           <strong style={{ color: '#007acc' }}>{hero.name} </strong>
           <br />
           <span style={{ fontSize: '14px' }}>
@@ -245,10 +268,6 @@ function App() {
           return powerA.localeCompare(powerB);
       }
     });
-    console.log('sorted');
-    sortedList.forEach((hero)=>{
-      console.log(hero.props.race);
-    })
     setSearchResults(sortedList);
   }
 
@@ -260,110 +279,125 @@ function App() {
 
   return (
     <>
-    <Router>
+      <Router>
         <Routes>
-          <Route path="/login" element ={<LogIn />}/>
-          <Route path="/signup" element ={<SignUp />}/>
-          <Route path="/" element ={
-          <div>
-            <nav>
-              <ul className="nav-links">
-                <li>
-                  <Link to = "/login" className="nav-button">Log In</Link>
-                </li>
-                <li>
-                  <Link to="/signup" className="nav-button">Sign Up</Link>
-                </li>
-              </ul>
-            </nav>
-            <div id="header">
-              {/* Search */}
-              <div id="searchContainer">
-                <h2>Search Superheroes</h2>
-                <form id="searchForm" onSubmit={handleSearch}>
-                  <label htmlFor="searchInput">Search by:</label>
-                  <select id="searchCategory">
-                    <option value="name">Name</option>
-                    <option value="race">Race</option>
-                    <option value="publisher">Publisher</option>
-                    <option value="power">Power</option>
-                  </select>
-                  <input type="text" id="searchInput" value={field} onChange={(e)=>setField(e.target.value)} placeholder="Search . . ." />
-                  <button type="submit" id="searchButton">Search</button>
-                </form>
-              </div>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          
+          <Route
+            path="/"
+            element={
+              
+              <div id="container">
+                <nav>
+                  <ul className="nav-links">
+                    <li>
+                      <Link to = "/login" className="nav-button">Log In</Link>
+                    </li>
+                    <li>
+                      <Link to="/signup" className="nav-button">Sign Up</Link>
+                    </li>
+                  </ul>
+                </nav>
+                <div id="topSection">
+                  {/* Top Left: Search Superheroes */}
+                  <div id="searchSuperheroes">
+                    <h2>Search Superheroes</h2>
+                    <form id="searchForm" onSubmit={handleSearch}>
+                      <label htmlFor="searchInput">Search by:</label>
+                      <select id="searchCategory">
+                        <option value="name">Name</option>
+                        <option value="race">Race</option>
+                        <option value="publisher">Publisher</option>
+                        <option value="power">Power</option>
+                      </select>
+                      <input
+                        type="text"
+                        id="searchInput"
+                        value={field}
+                        onChange={(e) => setField(e.target.value)}
+                        placeholder="Search . . ."
+                      />
+                      <button type="submit" id="searchButton">
+                        Search
+                      </button>
+                    </form>
+                  </div>
 
-              {/* Favourite Lists */}
-              <div id="favouriteListsContainer">
-                <h2>Favourite Lists</h2>
-                <button id="addListButton">Add List</button>
-                <form id="deleteForm">
-                  <label htmlFor="listNamesToDelete">Delete List:</label>
-                  <select id="listNamesToDelete">
-                    {favoriteLists.map((listName) => (
-                      <option key={listName}>{listName}</option>
-                    ))}
-                  </select>
-                  <button type="submit" id="deleteListButton">Delete</button>
-                </form>
-                {/* favourite lists will be displayed here as buttons*/}
-                <ul id="favouriteLists">
-                  {favoriteLists.map((listName) => (
-                    <button key={listName}>
-                      {listName}
-                    </button>
-                  ))}
-                </ul>
-              </div>
-            </div>
+                  {/* Top Right: Favorite Lists */}
+                  <div id="favoriteLists">
+                    <h2>Favourite Lists</h2>
+                    <button id="addListButton">Add List</button>
+                    <form id="deleteForm">
+                      <label htmlFor="listNamesToDelete">Delete List:</label>
+                      <select id="listNamesToDelete" className="select-styled">
+                        {favoriteLists.map((listName) => (
+                          <option key={listName}>{listName} </option>
+                        ))}
+                      </select>
+                      <button type="submit" id="deleteListButton">
+                        Delete
+                      </button>
+                    </form>
+                    <ul id="favouriteLists">
+                      {favoriteLists.map((listName) => (
+                        <button key={listName}>{listName}</button>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <div id="bottomSection">
+                {/* Bottom Left: Sort List */}
+                  <div id="sortList">
+                    <h2>Sort List: <span id="listName"></span></h2>
+                    <form id="listsForm">
+                      <label htmlFor="listNames">Add Selected Heroes to:</label>
+                      <div className="select-container">
+                        <select id="listNames" onChange={listAddNameChange} value={listNameToAdd} className="select-styled">
+                          {favoriteLists.map((listName) => (
+                            <option key={listName}>{listName}</option>
+                          ))}
+                        </select>
+                      </div>
+                      <button type="submit" id="listAddButton" onClick={addSelectedHeroesToList}>
+                        Add
+                      </button>
+                    </form>
+                    {/* Your existing buttons for sorting */}
+                  <button id="sortByName" onClick={() => { sortResults('name') }}>Sort by Name</button>
+                  <button id="sortByRace" onClick={() => { sortResults('race') }}>Sort by Race</button>
+                  <button id="sortByPublisher" onClick={() => { sortResults('publisher') }}>Sort by Publisher</button>
+                  <button id="sortByPower" onClick={() => { sortResults('power') }}>Sort by Power</button>
+                  <div id="resultsContainer">
+                  {/* Display Search Results */}
+                  <div id="searchResults" onClick={selectResults}>
+                    <p>{errorMessage}</p>
+                    {displaySearch()}
+                  </div>
+                  </div>
+                  </div>
 
-            {/* Display Superheroes in a List */}
-            <div id="listDetails">
-              {/* Modify the "Sort List" title section */}
-              <div id="sortListTitle">
-                <h2>Sort List: <span id="listName"></span></h2>
-                <form id="listsForm">
-                  <label htmlFor="listNames">Add Selected Heroes to:</label>
-                  <select id="listNames" onChange={listAddNameChange} value={listNameToAdd} >
-                    {favoriteLists.map((listName) => (
-                      <option key={listName}>{listName}</option>
-                    ))}
-                  </select>
-                  <button type="submit" id="listAddButton" onClick={addSelectedHeroesToList}>Add</button>
-                </form>
-              </div>
+                  {/* Bottom Right: Favorite Heroes */}
+                  <div id="favoriteHeroes">
+                    <h2>Favorite Heroes</h2>
+                    <div id="addedListsResults">
+                      <ul>Your Heroes will be displayed here...</ul>
+                    </div>
+                  </div>
+                </div>
 
-              {/* Add the "Favorite List Heroes" title section */}
-              <div id="favoriteListHeroesTitle">
-                <h2>Favorite List Heroes</h2>
-              </div>
-            </div>
-            <button id="sortByName" onClick={()=>{sortResults('name')}}>Sort by Name</button>
-            <button id="sortByRace" onClick={()=>{sortResults('race')}}>Sort by Race</button>
-            <button id="sortByPublisher" onClick={()=>{sortResults('publisher')}}>Sort by Publisher</button>
-            <button id="sortByPower" onClick={()=>{sortResults('power')}}>Sort by Power</button>
 
-            {/* Display Search Results */}
-            <div id="searchAndListsContainer">
-              {/* Search Results */}
-              <div id="searchResults" onClick={selectResults}>
-                <p>{errorMessage}</p>
-                {displaySearch()}
-                {/* Results will be displayed here */}
+                {/* Other sections go here */}
+                <div id="footer">
+                  <label htmlFor="FAQ">FAQ: What Publishers are available</label>
+                  <button id="FAQ" onClick={() => { displayPublishers() }}>Available Publishers</button>
+                </div>
+                <script src="script.js"></script>
               </div>
-              {/* Added Lists Results */}
-              <div id="addedListsResults">
-                <ul>Your Heroes will be displayed here...</ul>
-                {/* Results for added lists will be displayed here */}
-              </div>
-            </div>
-            <label htmlFor="FAQ">FAQ: What Publishers are available</label>
-            <button id="FAQ" onClick={()=>{displayPublishers()}}>Available Publishers</button>
-            <script src="script.js"></script>
-          </div>
-          } />
+            }
+          />
         </Routes>
-    </Router>
+      </Router>
     </>
   );
 }
